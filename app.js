8017e932328d12d1696f5af6d98f323f268356c4
Nikola Kosich <n.kosich@yandex.ru>
2023-07-31 01:33:09 +0300

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect(
-  'mongodb://127.0.0.1:27017/mestodb',
+  'mongodb://127.0.0.1:27017/bitfilmsdb',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
