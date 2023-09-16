a0f15fecfad266d9dcd78619656b3b34ede83f77
Nikola Kosich <n.kosich@yandex.ru>
2023-09-16 20:38:03 +0300

    deletecors

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('./middlewares/Cors')
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -27,7 +27,7 @@ app.use(helmet());
 
 app.use(bodyParser.json());
 
-app.use(cors);
+app.use(cors());
 
 app.use(requestLogger);
 
