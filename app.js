08ba444b5e754f81e2b3abce8876d343387757d9
Nikola Kosich <n.kosich@yandex.ru>
2023-10-31 00:36:18 +0300

    fixinge

--- app.js
+++ app.js
@@ -13,15 +13,11 @@ const router = require('./routes');
 
 const app = express();
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, NODE_ENV, DB_URL } = process.env;
 
-mongoose.connect(
-  DB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+mongoose.connect((NODE_ENV === 'production' ? DB_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb'), {
+  useNewUrlParser: true,
+});
 
 app.use(helmet());
 
