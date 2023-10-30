73152d021ca5176f012ee6bc287fba1b5fd4d994
Nikola Kosich <n.kosich@yandex.ru>
2023-10-30 23:05:49 +0300

    fixauth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,26 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const AuthorizationError = require('../errors/AuthorizationError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthorizationError('Не авторизованный пользователь');
+  const bearer = 'Bearer ';
+  const token = authorization.replace(bearer, '');
+
+  if (!token) {
+    return next(new AuthorizationError('Отсутствует токен'));
   }
 
   let payload;
 
-  const token = authorization.replace('Bearer ', '');
-
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secretKey');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new AuthorizationError('Не авторизованный пользователь'));
+    return next(new AuthorizationError('Не авторизирован'));
   }
 
   req.user = payload;
